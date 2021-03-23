a9f82908fa1b030f92952b0e7feaf7df11135579
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-23 17:02:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.0df662312c9a08926c476cc98be886ff.js"
 );
 
 self.addEventListener('message', (event) => {
